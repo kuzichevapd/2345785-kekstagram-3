7d2d9c1990ebedfd22a9c095689a082f866158f6
Polina <kuzicheva.pd@edu.spbstu.ru>
2023-03-29 17:19:00 +0300

    module5-task1

---
+++ js/util.js
@@ -0,0 +1,20 @@
+function getRandomInt(numberFrom, numberTo) {
+  if (numberFrom > numberTo || numberFrom < 0 || numberTo < 0) {
+    return 0;
+  }
+  if (numberFrom === numberTo) {
+    return numberFrom;
+  }
+  return Math.floor(numberFrom + (numberTo - numberFrom + 1) * Math.random());
+}
+
+function checkLength(str, len) {
+  len = Number(len);
+  str = String(str);
+  if (len < 0) {
+    return 0;
+  }
+  return str.length <= len;
+}
+
+export {getRandomInt, checkLength }
